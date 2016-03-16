aecbfa6ec9752cd1f03f9b832f9a66613ad35132
Sofia Andersson <sofia.m.andersson.a@gmail.com>
2016-03-16 08:51:44 +0100

    created leaderborditem class

---
+++ workout/LeaderbordItem.swift
@@ -0,0 +1,15 @@
+//
+//  LeaderbordItem.swift
+//  workout
+//
+//  Created by Sofia Andersson on 2016-03-16.
+//  Copyright © 2016 Sofia M Andersson. All rights reserved.
+//
+
+import Foundation
+
+class LeaderbordItem {
+    
+   
+    
+}
